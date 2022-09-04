6dfe53de7a998a194ca9838d6f5e1eda31eae161
“shaoqw” <cnpshao@gmail.com>
2022-09-04 18:14:24 +0800

    codeforce #817

---
+++ src/Codeforce Round #817/b_colourblindness.go
@@ -0,0 +1,40 @@
+// https://codeforces.com/contest/1722/problem/B
+// 将第一行字符中的所有'B'全部替换为字符'C'，然后比较两个字符是否一致
+
+package main
+
+import "fmt"
+
+func main() {
+	var t int
+	fmt.Scanf("%d\n", &t)
+	for i := 0; i < t; i++ {
+		var n int
+		//fmt.Scanf("%d\n", &n)
+		fmt.Scan(&n)
+		var str1, str2 string
+		//fmt.Scanf("%s\n", &str1)
+		fmt.Scan(&str1)
+		b1 := []byte(str1)
+		//fmt.Scanf("%s\n", &str2)
+		fmt.Scan(&str2)
+		b2 := []byte(str2)
+		for j := 0; j < n; j++ {
+			if b1[j] == 'B' {
+				b1[j] = 'G'
+			}
+			if b2[j] == 'B' {
+				b2[j] = 'G'
+			}
+		}
+		str1 = string(b1)
+		str2 = string(b2)
+		//fmt.Println(str)
+		//fmt.Println(str2)
+		if str2 == str1 {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+	}
+}
---
+++ src/Codeforce Round #817/c_word_game.go
@@ -0,0 +1,42 @@
+// https://codeforces.com/contest/1722/problem/C
+// 使用map，每一个新增的字符都将写入map中，然后遍历每一个人计算其对应分数
+
+package main
+
+import "fmt"
+
+func main() {
+	var t int
+	fmt.Scanf("%d\n", &t)
+	for i := 1; i <= t; i++ {
+		var n int
+		mp := make(map[string]int)
+		//var mp map[string]int
+		fmt.Scanf("%d\n", &n)
+		//var a [3][n]string
+		a := make([][]string, 3)
+		for j := 0; j < 3; j++ {
+			a[j] = make([]string, n)
+			for k := 0; k < n; k++ {
+				//var str string
+				//fmt.Scan(&a[j][k])
+				fmt.Scanf("%s", &a[j][k])
+				//a[i][j] = str
+				//fmt.Println("_", len(a[i][j]), "_", a[i][j], "_")
+				mp[a[j][k]]++
+			}
+		}
+		for j := 0; j < 3; j++ {
+			total := 0
+			for k := 0; k < n; k++ {
+				if mp[a[j][k]] == 1 {
+					total += 3
+				} else if mp[a[j][k]] == 2 {
+					total++
+				}
+			}
+			fmt.Printf("%d ", total)
+		}
+		fmt.Printf("\n")
+	}
+}
